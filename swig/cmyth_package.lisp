51d8b92fb325ed4b00bc32bdd601164b2eea9037
Jon Gettler <gettler@mvpmc.org>
2012-06-11 17:19:51 -0500

    Misc updates to the Common Lisp bindings and test program.

--- swig/cmyth_package.lisp
+++ swig/cmyth_package.lisp
@@ -20,6 +20,9 @@
 ;;;;
 ;;;; cmyth package
 ;;;;
+;;;; This package implements an API to libcmyth and librefmem, which allows
+;;;; access to a MythTV backend.
+;;;;
 
 (require 'asdf)
 (require 'cffi)
@@ -27,21 +30,26 @@
 (defpackage :cmyth
   (:use :common-lisp :cffi)
   (:export
+   ;; macros
+   :with-connection :with-proglist :with-progs :with-proginfo
+   :with-open-program
    ;; common methods
    :release
    ;; refmem functions
    :get-refs :get-bytes :ref-release
    ;; connection class
-   :new-connection :protocol-version :get-proglist
+   :new-connection :protocol-version :get-proglist :get-progs
    :storage-space-total :storage-space-used
    ;; proglist class
    :get-count :get-prog
    ;; proginfo class
+   :item
    :title :subtitle :description
    :open-file
    ;; file class
-   :bytes :seek :offset :read-bytes :buf
-   ))
+   :bytes :seek :offset :read-bytes :buf :buflen
+   ;; exception
+   :exception :text))
 
 (in-package :cmyth)
 
@@ -84,6 +92,9 @@
 
 (defun ref-release (ptr)
   (ref_release ptr))
+
+(define-condition exception (error)
+  ((text :initarg :text :reader text)))
 
 ;;;
 ;;; file class
@@ -144,17 +155,20 @@
 ;;;
 (defclass proginfo ()
   ((pinfo :initarg :pinfo :accessor pinfo)
-   (cbl :initarg :cbl :accessor cbl :initform nil))
+   (cbl :initarg :cbl :accessor cbl :initform nil)
+   (hash :initarg :hash :accessor hash))
   (:documentation "cmyth program info class"))
 
 (defgeneric release (proginfo))
 (defgeneric equals (proginfo proginfo))
+(defgeneric item (proginfo which))
 (defgeneric port (proginfo))
 (defgeneric seconds (proginfo))
 (defgeneric bytes (proginfo))
 (defgeneric title (proginfo))
 (defgeneric subtitle (proginfo))
 (defgeneric description (proginfo))
+(defgeneric path-name (proginfo))
 (defgeneric open-file (proginfo))
 
 (defmethod release ((p proginfo))
@@ -166,11 +180,23 @@
       t
       nil))
 
+(defmethod item ((p proginfo) which)
+  (gethash which (hash p)))
+
 (defun get-string (func p)
   (let* ((str (funcall func (pinfo p)))
 	 (ret (foreign-string-to-lisp str)))
     (ref_release str)
     ret))
+
+(defmethod port ((p proginfo))
+  (cmyth_proginfo_port (pinfo p)))
+
+(defmethod seconds ((p proginfo))
+  (cmyth_proginfo_length_sec (pinfo p)))
+
+(defmethod bytes ((p proginfo))
+  (cmyth_proginfo_length (pinfo p)))
 
 (defmethod title ((p proginfo))
   (get-string #'cmyth_proginfo_title p))
@@ -181,15 +207,30 @@
 (defmethod description ((p proginfo))
   (get-string #'cmyth_proginfo_description p))
 
+(defmethod path-name ((p proginfo))
+  (get-string #'cmyth_proginfo_pathname p))
+
 (defmethod open-file ((p proginfo))
   (new-file (pinfo p)))
 
+(defun add-hash (p name &optional (fname name))
+  (setf (gethash name (hash p)) (funcall fname p)))
+
 (defun new-proginfo (conn plist which)
   (let* ((pinfo (cmyth_proglist_get_item plist which))
-	 (cbl (cmyth_get_commbreaklist conn pinfo)))
+	 (cbl (cmyth_get_commbreaklist conn pinfo))
+	 (hash (make-hash-table)))
     (if (pointer-eq pinfo (null-pointer))
 	nil
-	(make-instance 'proginfo :pinfo pinfo :cbl cbl))))
+	(let ((p (make-instance 'proginfo :pinfo pinfo :cbl cbl :hash hash)))
+	  (add-hash p 'port)
+	  (add-hash p 'seconds)
+	  (add-hash p 'bytes)
+	  (add-hash p 'title)
+	  (add-hash p 'subtitle)
+	  (add-hash p 'description)
+	  (add-hash p 'pathname 'path-name)
+	  p))))
 
 ;;;
 ;;; proglist class
@@ -233,6 +274,7 @@
 (defgeneric connect (connection))
 (defgeneric protocol-version (connection))
 (defgeneric get-proglist (connection))
+(defgeneric get-progs (connection))
 (defgeneric storage-space (connection))
 
 (defmethod release ((c connection))
@@ -252,6 +294,16 @@
 
 (defmethod get-proglist ((c connection))
   (new-proglist (conn c)))
+
+(defmethod get-progs ((c connection))
+  (let* ((plist (cmyth_proglist_get_all_recorded (conn c)))
+	 (progs (if (pointer-eq plist (null-pointer))
+		    nil
+		    (loop for i from 0 below
+			 (cmyth_proglist_get_count plist) collect
+			 (new-proginfo (conn c) plist i)))))
+    (ref_release plist)
+    progs))
 
 (defmethod storage-space ((c connection))
   (let ((total (foreign-alloc :long-long))
@@ -275,4 +327,59 @@
 			  :buflen buflen :rcvbuf rcvbuf)))
     (if (connect c)
 	c
-	nil)))
+	(error 'exception :text "Connection failed"))))
+
+;;;;
+;;;; with- macros
+;;;;
+(defmacro with-connection ((conn host) &body body)
+  (let ((local (gensym)))
+    `(let (,conn ,local)
+       (unwind-protect
+	    (progn (setq ,local (new-connection ,host)
+			 ,conn ,local)
+		   ,@body)
+	 (unless (null ,local)
+	   (release ,local))))))
+
+(defmacro with-proglist ((pl conn) &body body)
+  (let ((local (gensym)))
+    `(let (,pl ,local)
+       (unwind-protect
+	    (progn (setq ,local (new-proglist (conn ,conn))
+			 ,pl ,local)
+		   ,@body)
+	 (unless (null ,local)
+	   (release ,local))))))
+
+(defmacro with-proginfo ((p proglist which) &body body)
+  (let ((local (gensym)))
+    `(let (,p ,local)
+       (unwind-protect
+	    (progn (setq ,local (new-proginfo (conn ,proglist) (plist ,proglist)
+					      ,which)
+			 ,p ,local)
+		   ,@body)
+	 (unless (null ,local)
+	   (release ,local))))))
+
+(defmacro with-progs ((progs conn) &body body)
+  (let ((local (gensym)))
+    `(let (,progs ,local)
+       (unwind-protect
+	    (progn (setq ,local (get-progs ,conn)
+			 ,progs ,local)
+		   ,@body)
+	 (unless (null ,local)
+	   (loop while (> (length ,local) 0) do
+		(release (pop ,local))))))))
+
+(defmacro with-open-program ((f p) &body body)
+  (let ((local (gensym)))
+    `(let (,f ,local)
+       (unwind-protect
+	    (progn (setq ,local (open-file ,p)
+			 ,f ,local)
+		   ,@body)
+	 (unless (null ,local)
+	   (release ,local))))))
