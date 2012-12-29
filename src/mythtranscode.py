5f75978f3bd3f9f0118613c109f0000ae7e02d0d
Jon Gettler <gettler@mvpmc.org>
2012-12-29 15:37:57 -0600

    A bunch of misc changes to the Python sample apps.

--- src/mythtranscode.py
+++ src/mythtranscode.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
 #
+# Copyright (C) 2012, Jon Gettler
+# http://www.mvpmc.org/
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+#
 # Transcode MythTV recordings into a format that seems to work well on all my
-# various devices, and takes up far less space than the originals.  A description
-# is added to the output file, along with chapter markers that match the original
-# commercial skip points, although many media players do not support these mpeg4
-# extentions.
+# various devices, and takes up far less space than the originals.  A
+# description is added to the output file, along with chapter markers that
+# match the original commercial skip points, although many media players do
+# not support these mpeg4 extentions.
 #
 # The user provides the filename to transcode, and all the MythTV information
 # is gathered from the MythTV backend via libcmyth.
@@ -13,9 +32,12 @@
 #
 #     mythtranscode.py -p %DIR%/%FILE% -r
 #
-# This script requires the following tools:
+# This script requires the following tool:
 #
 #     mencoder
+#
+# This following tools are optional, but highly recommended:
+#
 #     MP4Box
 #     mp4tags
 #
@@ -32,12 +54,6 @@ cmd_encode = 'mencoder %s -o %s -vf dsize=720:480:2,scale=-8:-8,harddup -oac faa
 cmd_chapters = 'MP4Box -chap %s -tmp %s %s'
 cmd_tags = 'mp4tags -c %s %s'
 
-server = None
-filename = None
-dirname = None
-outdir = None
-output = None
-replace = False
 verbose = False
 
 def usage(code):
@@ -45,30 +61,189 @@ def usage(code):
     Print user help information.
     """
     print 'mythtranscode: <options>'
-    print '        -h           --help       print this help'
-    print '        -o <dir>     --output     output directory'
-    print '        -p <file>    --program    program to transcode'
-    print '        -r           --replace    replace original file'
-    print '        -s <host>    --server     MythTV host'
-    print '        -v           --verbose    verbose output'
+    print '        -d <password>  --database   MySQL password'
+    print '        -h             --help       print this help'
+    print '        -o <dir>       --output     output directory'
+    print '        -p <file>      --program    program to transcode'
+    print '        -r             --replace    replace original file'
+    print '        -s <host>      --server     MythTV host'
+    print '        -v             --verbose    verbose output'
     sys.exit(code)
 
-def parse_opts():
-    global outdir
-    global filename
-    global dirname
-    global replace
-    global server
+def log(msg):
     global verbose
 
+    if verbose:
+        print msg
+
+def binary(program):
+    for path in os.environ["PATH"].split(os.pathsep):
+        path = path.strip('"')
+        fpath = os.path.join(path, program)
+        if os.path.isfile(fpath) and os.access(fpath, os.X_OK):
+            return True
+    return None
+
+def timecode(marker):
+    divisor = 60
+    frames = int((marker % divisor) / 2)
+    marker = int(marker / divisor)
+    seconds = int(marker % 60)
+    marker = int(marker / 60)
+    minutes = int(marker % 60)
+    marker = int(marker / 60)
+    hours = marker
+    return hours,minutes,seconds,int(frames*33.3333)
+
+def transcode(prog, dirname, filename, output):
+    p = '%s/%s' % (dirname,filename)
+    log('Transcode %s to %s.' % (p, output))
+    convert = subprocess.Popen(cmd_encode % (p, output),
+                               shell=True,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stdout_value, stderr_value = convert.communicate()
+    if convert.wait() != 0:
+        print 'Error: Transcode failed: %s' % stderr_value
+        sys.exit(-1)
+
+def add_chapters(prog, dirname, output):
+    if not binary('MP4Box'):
+        log('MP4Box not found, not adding commercial breaks.')
+        return
+    log('Add %d commercial breaks.' % prog.commercial_count())
+    f = tempfile.NamedTemporaryFile()
+    n = 1
+    for i in range(prog.commercial_count()):
+        hours,minutes,seconds,ms = timecode(prog.commercial_start(i))
+        f.write('CHAPTER%02d=%.2d:%.2d:%.2d.%.3d\n' %
+                (n, hours, minutes, seconds, ms))
+        f.write('CHAPTER%02dNAME=Start\n' % (n))
+        hours,minutes,seconds,ms = timecode(prog.commercial_end(i))
+        n = n + 1
+        f.write('CHAPTER%02d=%.2d:%.2d:%.2d.%.3d\n' %
+                (n, hours, minutes, seconds, ms))
+        f.write('CHAPTER%02dNAME=End\n' % (n))
+        n = n + 1
+    f.flush()
+    chapters = subprocess.Popen(cmd_chapters % (f.name, dirname, output),
+                                shell=True,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    stdout_value, stderr_value = chapters.communicate()
+    if chapters.wait() != 0:
+        log('Error: failed to add chapter markers: %s' % stderr_value)
+    f.close()
+
+def add_tags(prog, output):
+    if not binary('mp4tags'):
+        log('mp4tags not found, not adding mpeg4 tags.')
+        return
+    log('Add mpeg4 tags.')
+    title = prog.title()
+    subtitle = prog.subtitle()
+    chan0 = prog.channel_string()
+    chan1 = prog.channel_sign()
+    desc = prog.description()
+    comment = '%s: %s - %s %s - %s' % (title, subtitle, chan0, chan1, desc)
+    tags = subprocess.Popen(cmd_tags % (re.escape(comment), output),
+                            shell=True,
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout_value, stderr_value = tags.communicate()
+    if tags.wait() != 0:
+        log('Error: failed to add tags: %s' % stderr_value)
+
+def replace_original(dirname, filename, output, server, db_passwd):
+    log('Replace original file and update the database.')
+
+    # If the password is not provided, assume we can get it from /etc.
+    if db_passwd == None:
+        f = open('/etc/mythtv/mysql.txt', 'r')
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith('DBPassword'):
+                db_passwd = (line.split('=')[1])[:-1]
+        f.close()
+
+    if db_passwd == None:
+        print 'Error: MySQL password is unknown!'
+        sys.exit(-1)
+
+    # replace file
+    oldsize = os.path.getsize('%s/%s' % (dirname, filename))
+    cp = subprocess.Popen('mv %s/%s %s/%s.orig' % (dirname, filename, dirname, filename),
+                          shell=True,
+                          stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
+    stdout_value, stderr_value = cp.communicate()
+    if cp.wait() != 0:
+        print 'Error: failed to replace file: %s' % stderr_value
+        sys.exit(-1)
+    cp = subprocess.Popen('cp %s %s/%s' % (output, dirname, filename),
+                          shell=True,
+                          stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
+    stdout_value, stderr_value = cp.communicate()
+    if cp.wait() != 0:
+        print 'Error: failed to replace file: %s' % stderr_value
+        sys.exit(-1)
+
+    # run mythcommflag --rebuild to rebuild the seektable
+    if server == 'localhost':
+        mcf = subprocess.Popen('mythcommflag -f %s/%s --rebuild' % (dirname, filename),
+                               shell=True,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        stdout_value, stderr_value = mcf.communicate()
+        if mcf.wait() != 0:
+            print 'Error: mythcommflag failed: %s' % stderr_value
+            sys.exit(-1)
+    else:
+        log('Run "mythcommflag -f %s --rebuild" on the server.' % filename)
+
+    # run mysql to change the filesize
+    size = os.path.getsize('%s/%s' % (dirname, filename))
+    log('file size changed from %d to %d' % (oldsize, size))
+    sql = "UPDATE\nrecorded\nSET\nfilesize = %d\nWHERE\nbasename = '%s';\n" % (size,filename)
+    mysql = subprocess.Popen('mysql -h %s -u mythtv -p%s mythconverg' % (server, db_passwd),
+                             shell=True,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+    mysql.stdin.write(sql)
+    stdout_value, stderr_value = mysql.communicate()
+    mysql.stdin.close()
+    if mysql.wait() != 0:
+        print 'Error: mysql update failed: %s' % stderr_value
+
+def main():
+    global verbose
+
+    server = None
+    filename = None
+    dirname = None
+    outdir = None
+    output = None
+    replace = False
+    password = None
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:p:rs:v",
-                                   [ "help", "output=", "program=",
-                                 "replace", "server=", 'verbose' ])
+        opts, args = getopt.getopt(sys.argv[1:], "d:ho:p:rs:v",
+                                   [ "database=", "help", "output=", "program=",
+                                     "replace", "server=", 'verbose' ])
     except getopt.GetoptError:
         usage(1)
 
     for o, a in opts:
+        if o in ("-d", "--database"):
+            password = a
         if o in ("-h", "--help"):
             usage(0)
         if o in ("-o", "--output"):
@@ -99,158 +274,6 @@ def parse_opts():
         else:
             outdir = os.getcwd()
 
-def log(msg):
-    global verbose
-
-    if verbose:
-        print msg
-
-def timecode(marker):
-    divisor = 60
-    frames = int((marker % divisor) / 2)
-    marker = int(marker / divisor)
-    seconds = int(marker % 60)
-    marker = int(marker / 60)
-    minutes = int(marker % 60)
-    marker = int(marker / 60)
-    hours = marker
-    return hours,minutes,seconds,int(frames*33.3333)
-
-def transcode(prog):
-    global dirname
-    global filename
-    global output
-    p = '%s/%s' % (dirname,filename)
-    log('Transcode %s to %s.' % (p, output))
-    convert = subprocess.Popen(cmd_encode % (p, output),
-                               shell=True,
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    stdout_value, stderr_value = convert.communicate()
-    if convert.wait() != 0:
-        print 'Error: Transcode failed: %s' % stderr_value
-        sys.exit(-1)
-
-def add_chapters(prog):
-    global dirname
-    global output
-
-    cbl = prog.commbreaklist()
-    log('Add %d commercial breaks.' % len(cbl))
-    f = tempfile.NamedTemporaryFile()
-    n = 1
-    for i in range(prog.commercial_count()):
-        hours,minutes,seconds,ms = timecode(prog.commercial_start(i))
-        f.write('CHAPTER%02d=%.2d:%.2d:%.2d.%.3d\n' %
-                (n, hours, minutes, seconds, ms))
-        f.write('CHAPTER%02dNAME=Start\n' % (n))
-        hours,minutes,seconds,ms = timecode(prog.commercial_end(i))
-        n = n + 1
-        f.write('CHAPTER%02d=%.2d:%.2d:%.2d.%.3d\n' %
-                (n, hours, minutes, seconds, ms))
-        f.write('CHAPTER%02dNAME=End\n' % (n))
-        n = n + 1
-    f.flush()
-    chapters = subprocess.Popen(cmd_chapters % (f.name, dirname, output),
-                                shell=True,
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-    stdout_value, stderr_value = chapters.communicate()
-    if chapters.wait() != 0:
-        print 'Error: failed to add chapter markers: %s' % stderr_value
-        sys.exit(-1)
-    f.close()
-
-def add_tags(prog):
-    global output
-
-    log('Add mpeg4 tags.')
-    title = prog.title()
-    subtitle = prog.subtitle()
-    chan0 = prog.chanstr()
-    chan1 = prog.chansign()
-    desc = prog.description()
-    comment = '%s: %s - %s %s - %s' % (title, subtitle, chan0, chan1, desc)
-    tags = subprocess.Popen(cmd_tags % (re.escape(comment), output),
-                            shell=True,
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-    stdout_value, stderr_value = tags.communicate()
-    if tags.wait() != 0:
-        print 'Error: failed to add tags: %s' % stderr_value
-        sys.exit(-1)
-
-def replace_original():
-    global dirname
-    global filename
-    global output
-
-    log('Replace original file and update the database.')
-
-    f = open('/etc/mythtv/mysql.txt', 'r')
-    lines = f.readlines()
-    for line in lines:
-        if line.startswith('DBPassword'):
-            db_passwd = (line.split('=')[1])[:-1]
-    f.close()
-
-    # replace file
-    oldsize = os.path.getsize('%s/%s' % (dirname, filename))
-    cp = subprocess.Popen('mv %s/%s %s/%s.orig' % (dirname, filename, dirname, filename),
-                          shell=True,
-                          stdin=subprocess.PIPE,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE)
-    stdout_value, stderr_value = cp.communicate()
-    if cp.wait() != 0:
-        print 'Error: failed to replace file: %s' % stderr_value
-        sys.exit(-1)
-    cp = subprocess.Popen('cp %s %s/%s' % (output, dirname, filename),
-                          shell=True,
-                          stdin=subprocess.PIPE,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE)
-    stdout_value, stderr_value = cp.communicate()
-    if cp.wait() != 0:
-        print 'Error: failed to replace file: %s' % stderr_value
-        sys.exit(-1)
-    # run mythcommflag --rebuild
-    mcf = subprocess.Popen('mythcommflag -f %s/%s --rebuild' % (dirname, filename),
-                           shell=True,
-                           stdin=subprocess.PIPE,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE)
-    stdout_value, stderr_value = mcf.communicate()
-    if mcf.wait() != 0:
-        print 'Error: mythcommflag failed: %s' % stderr_value
-        sys.exit(-1)
-    # run mysql to change the filesize
-    size = os.path.getsize('%s/%s' % (dirname, filename))
-    log('file size changed from %d to %d' % (oldsize, size))
-    sql = "UPDATE\nrecorded\nSET\nfilesize = %d\nWHERE\nbasename = '%s';\n" % (size,filename)
-    mysql = subprocess.Popen('mysql -u mythtv -p%s mythconverg' % db_passwd,
-                             shell=True,
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-    mysql.stdin.write(sql)
-    stdout_value, stderr_value = mysql.communicate()
-    mysql.stdin.close()
-    if mysql.wait() != 0:
-        print 'Error: mysql update failed: %s' % stderr_value
-
-def main():
-    global outdir
-    global filename
-    global dirname
-    global server
-    global output
-
-    parse_opts()
-
     output = '%s/%s.mp4' % (outdir, filename)
 
     conn = cmyth.connection(server)
@@ -261,11 +284,11 @@ def main():
         prog = list.get_prog(i)
         pathname = prog.pathname()
         if pathname[1:] == filename:
-            transcode(prog)
-            add_chapters(prog)
-            add_tags(prog)
+            transcode(prog, dirname, filename, output)
+            add_chapters(prog, dirname, output)
+            add_tags(prog, output)
             if replace == True:
-                replace_original()
+                replace_original(dirname, filename, output, server, password)
 
 if __name__ == "__main__":
     main()
